e84c02b9016fc79294d8f8d730337ac0a51e1aaa
lekolawole <lekolawole@gmail.com>
2022-05-16 19:47:08 -0400

    Testing UpdateUser( )

--- src/components/profile-view/updated-user.jsx
+++ src/components/profile-view/updated-user.jsx
@@ -18,7 +18,7 @@ export class UpdatedUser extends React.Component {
     e.preventDefault();
 
     this.setState({
-      collapseMenu: !this.state.collapseMenu
+      collapseMenu: this.state.collapseMenu
     });
   }
 
@@ -41,7 +41,57 @@ export class UpdatedUser extends React.Component {
       console.log(e))
   }
   
-  
+  updateUser = (e) => {
+    e.preventDefault();
+    const isReq = validate();
+
+    const validate = () => {
+    let isReq = true;
+    if(!username){
+      setUsernameErr('Username is Required');
+      isReq = false;
+    }else if(username.length < 2){
+      setUsernameErr('Username must be 2 characters long');
+      isReq = false;
+    }
+    if(!password){
+      setPasswordErr('Password is Reaquired');
+      isReq = false;
+    }else if(password.length < 8){
+      setPasswordErr('Password must be 8 characters long');
+      isReq = false;
+    }
+    if(!email){
+      setEmailErr('Email is Required');
+      isReq = false;
+    }else if(email.indexOf('@') === -1) {
+      setEmailErr('Must be a valid email address');
+      isReq = false;
+    }
+
+    return isReq;
+  };
+
+    if(isReq){
+        axios.post(`https://my-flix-22.herokuapp.com/users/{user}`, {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      })
+      .then(response => {
+        console.log(response.data);
+        alert('Profile changes were saved!');
+      })
+      .catch(e => {
+        console.log('Error during update.');
+        alert('Changes not saved')
+      });
+    }
+    
+    console.log(username, password, email, birthday);
+  };
+
   render() {
     const user = localStorage.getItem("user");
     let email = localStorage.getItem("email");
@@ -56,28 +106,27 @@ export class UpdatedUser extends React.Component {
      <Row>
        <Col>
         <Button
-            onClick={this.showHide}
+            onClick={!this.showHide}
             aria-controls="example-collapse-text"
             role="button"
-            aria-expanded="false"
-            >
-              //Click to Manage Profile
+            aria-expanded="false">
+            Click to Manage Profile
           </Button>
         </Col>
         <Col>
-          <Collapse in={this.state.collapseMenu}>
+          {/* <Collapse in={!this.state.collapseMenu}>
        
-          <Form id="example-collapse-text">
-            <Form.Group>
-              <Form.Label>Username </Form.Label>
-              <Form.Control 
-                style={{ "width":"24rem", "display":"flex"}}
-                  type="text" 
-                  value={user} 
-                  placeholder= 'Enter a username' 
-                  onChange={e => setUsername(e.target.value)}
-                  />
-            </Form.Group>
+            <Form id="example-collapse-text">
+              <Form.Group>
+                <Form.Label>Username </Form.Label>
+                <Form.Control 
+                  style={{ "width":"24rem", "display":"flex"}}
+                    type="text" 
+                    value={user} 
+                    placeholder= 'Enter a username' 
+                  
+                    />
+              </Form.Group>
 
             <Form.Group>
               <Form.Label>Email </Form.Label>
@@ -86,7 +135,6 @@ export class UpdatedUser extends React.Component {
                   type="text" 
                   value={email} 
                   placeholder= 'Enter a username' 
-                  onChange={e => setUsername(e.target.value)}
                   />
             </Form.Group>
 
@@ -97,7 +145,6 @@ export class UpdatedUser extends React.Component {
                   type="text" 
                   value={password} 
                   placeholder= {password} 
-                  onChange={e => setUsername(e.target.value)}
                   />
             </Form.Group>
 
@@ -108,11 +155,14 @@ export class UpdatedUser extends React.Component {
                   type="text" 
                   value={birthday} 
                   placeholder= 'Enter a username' 
-                  onChange={e => setUsername(e.target.value)}
                   />
             </Form.Group>
+            <Button 
+              type="submit"
+              onClick={this.updateUser()}
+            >Save Changes</Button>
           </Form>
-        </Collapse>
+        </Collapse> */}
        </Col>
      </Row>
    </Container>
